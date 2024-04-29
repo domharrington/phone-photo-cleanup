6f7c7b39a51918db75b35f1639f53caf95429e5f
Dom Harrington <domharrington@protonmail.com>
2024-04-29 14:36:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# phone-photo-cleanup
+
+Open [OpenMTP](https://github.com/ganeshrvel/openmtp) and navigate to DCIM/Camera
+
+Multiselect a few hundred photos (this is slow and buggy right now)
+
+Drag them over to `./`
+
+Run `./check.sh`
+
+If a photo or video cannot be found in `~/Pictures`, it will print it out.
+
+Run `DELETE=true ./check.sh` to cleanup files that we know can be found.
+
+For the ones that can't be found, drag them over into the WebDAV folder: http://admin@raspberrypi:2342/import/ - this will import them into photoprism and eventually Syncthing them into ~/Pictures.
+
+In a few minutes you should be able to run `DELETE=true ./check.sh` again and it'll clear up the ones that have now been imported.
---
+++ check.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+for i in $(ls *.jpg); do
+  count=$(find ~/Pictures -name $i | wc -l)
+  if [ $count -eq 0 ]; then
+    echo "$i not found"
+  else
+    if [ $DELETE ]; then
+      echo "$i found, deleting"
+      rm $i
+    else
+      echo "$i found"
+    fi
+  fi
+done
+
+for i in $(ls *.mp4); do
+  count=$(find ~/Pictures -name $i | wc -l)
+  if [ $count -eq 0 ]; then
+    echo "$i not found"
+  else
+    if [ $DELETE ]; then
+      echo "$i found, deleting"
+      rm $i
+    else
+      echo "$i found"
+    fi
+  fi
+done
+
+# current total: 7688
+# current total: 7505
+# current total: 7310
+
+# Missing files from ~/Pictures
+
+# 20210916_081525.jpg not found
+# 20210916_095310.jpg not found
+# 20210916_095319.jpg not found
+# 20210916_131335.jpg not found
+# 20210916_132902.jpg not found
+# 20210916_193850.jpg not found
