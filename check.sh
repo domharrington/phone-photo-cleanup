84c8b8c09c872014346afdf08139cb010c9dc5d7
Dom Harrington <domharrington@protonmail.com>
2024-05-06 12:36:57 +0100

    Switch to using `find` for everything
    Using the result of ls for the loop had problems with spaces in filenames

--- check.sh
+++ check.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
 set -e
+# Add this to see all of the commands being run
+# set -x
 
-for i in $(ls *.jpg); do
-  count=$(find ~/Pictures -name $i | wc -l)
+find . -iname "*.jpg" -exec basename "{}" \; | while read i
+do
+  count=$(find ~/Pictures -name "$i" | wc -l)
   if [ $count -eq 0 ]; then
     echo "$i not found"
   else
     if [ $DELETE ]; then
       echo "$i found, deleting"
-      rm $i
+      find . -iname "$i" -delete
     else
-      echo "$i found $(find ~/Pictures -name $i)"
+      echo "$i found $(find ~/Pictures -name "$i")"
     fi
   fi
 done
 
-for i in $(ls *.mp4); do
-  count=$(find ~/Pictures -name $i | wc -l)
+find . -iname "*.mp4" -exec basename "{}" \; | while read i
+do
+  count=$(find ~/Pictures -name "$i" | wc -l)
   if [ $count -eq 0 ]; then
     echo "$i not found"
   else
     if [ $DELETE ]; then
       echo "$i found, deleting"
-      rm $i
+      find . -iname "$i" -delete
     else
-      echo "$i found $(find ~/Pictures -name $i)"
+      echo "$i found $(find ~/Pictures -name "$i")"
     fi
   fi
 done
