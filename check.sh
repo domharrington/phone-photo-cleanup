9430716774559e47a0ba7301f4cf733d2812c1b8
Dom Harrington <domharrington@protonmail.com>
2024-05-06 12:37:05 +0100

    Add support for .mov files

--- check.sh
+++ check.sh
@@ -34,3 +34,17 @@ do
   fi
 done
 
+find . -iname "*.mov" -exec basename "{}" \; | while read i
+do
+  count=$(find ~/Pictures -name "$i" | wc -l)
+  if [ $count -eq 0 ]; then
+    echo "$i not found"
+  else
+    if [ $DELETE ]; then
+      echo "$i found, deleting"
+      find . -iname "$i" -delete
+    else
+      echo "$i found $(find ~/Pictures -name "$i")"
+    fi
+  fi
+done
